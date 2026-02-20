8ae511e9ce0bcdd36317e4e25d3b1b1221e23b39
PengTianxiang <932328811@qq.com>
2026-02-20 23:54:04 +0800

    20260220 Third

--- Q2.py
+++ Q2.py
@@ -17,21 +17,26 @@ class Joint:
         print(matrix)
         return matrix
 
+def caculate_matrixes(inputs):
+    joint_angle = list(map(float, inputs.split()))
+    joint_angle[1] -= 90
+
+    shoulder_pan = Joint(0, 0, 0.0624, joint_angle[0])
+    shoulder_lift = Joint(-90, 0.035, 0, joint_angle[1])
+    elbow_flex = Joint(0, 0.116, 0, joint_angle[2])
+    wrist_flex = Joint(0, 0.135, 0, joint_angle[3])
+    wrist_roll = Joint(-90, 0, 0.061, joint_angle[4] )
+
+    shoulder_pan_trans = shoulder_pan.trans_matrix()
+    shoulder_lift_trans = shoulder_lift.trans_matrix()
+    elbow_flex_trans = elbow_flex.trans_matrix()
+    wrist_flex_trans = wrist_flex.trans_matrix()
+    wrist_roll_trans = wrist_roll.trans_matrix()
+
+    final_matrix = shoulder_pan_trans@shoulder_lift_trans@elbow_flex_trans@wrist_flex_trans@wrist_roll_trans
+    print(final_matrix)
+
 inputs = input("Please input the angle")
-joint_angle = list(map(float, inputs.split()))
-joint_angle[1] -= 90
 
-shoulder_pan = Joint(0, 0, 0.0624, joint_angle[0])
-shoulder_lift = Joint(-90, 0.035, 0, joint_angle[1])
-elbow_flex = Joint(0, 0.116, 0, joint_angle[2])
-wrist_flex = Joint(0, 0.135, 0, joint_angle[3])
-wrist_roll = Joint(-90, 0, 0.061, joint_angle[4] )
-
-shoulder_pan_trans = shoulder_pan.trans_matrix()
-shoulder_lift_trans = shoulder_lift.trans_matrix()
-elbow_flex_trans = elbow_flex.trans_matrix()
-wrist_flex_trans = wrist_flex.trans_matrix()
-wrist_roll_trans = wrist_roll.trans_matrix()
-
-final_matrix = shoulder_pan_trans@shoulder_lift_trans@elbow_flex_trans@wrist_flex_trans@wrist_roll_trans
-print(final_matrix)
+caculate_matrixes(inputs)
+    
