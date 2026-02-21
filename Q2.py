a0a7ba21abe35aab05693f669d1af8b03f175859
PengTianxiang <932328811@qq.com>
2026-02-21 22:13:23 +0800

    20260221 First

--- Q2.py
+++ Q2.py
@@ -1,7 +1,8 @@
 import numpy as np
+import roboticstoolbox as rtb
 
 class Joint:
-    def __init__(self, Link_twist, Link_length, Link_offset, Joint_angle):
+    def __init__(self, Link_twist, Link_length, Link_offset, Joint_angle=0):
         self.Link_twist = np.radians(Link_twist)
         self.Link_length = Link_length
         self.Link_offset = Link_offset
@@ -16,10 +17,14 @@ class Joint:
         ])
         print(matrix)
         return matrix
+    
+    def assemble_joint(self):
+        return rtb.RevoluteMDH(alpha=self.Link_twist, a=self.Link_length, d=self.Link_offset)
 
+"""定义计算函数"""
 def caculate_matrixes(inputs):
     joint_angle = list(map(float, inputs.split()))
-    joint_angle[1] -= 90
+    joint_angle[1] -= 90                              #预处理输入数据
 
     shoulder_pan = Joint(0, 0, 0.0624, joint_angle[0])
     shoulder_lift = Joint(-90, 0.035, 0, joint_angle[1])
@@ -27,16 +32,19 @@ def caculate_matrixes(inputs):
     wrist_flex = Joint(0, 0.135, 0, joint_angle[3])
     wrist_roll = Joint(-90, 0, 0.061, joint_angle[4] )
 
+    """计算并打印变换矩阵"""
     shoulder_pan_trans = shoulder_pan.trans_matrix()
     shoulder_lift_trans = shoulder_lift.trans_matrix()
     elbow_flex_trans = elbow_flex.trans_matrix()
     wrist_flex_trans = wrist_flex.trans_matrix()
     wrist_roll_trans = wrist_roll.trans_matrix()
 
+    """计算并输出总变换矩阵"""
     final_matrix = shoulder_pan_trans@shoulder_lift_trans@elbow_flex_trans@wrist_flex_trans@wrist_roll_trans
     print(final_matrix)
+    return final_matrix
 
 inputs = input("Please input the angle")
 
-caculate_matrixes(inputs)
+output_matirx = caculate_matrixes(inputs)
     
