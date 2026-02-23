b88d0c864a3460f5e7bab64709cc2ed57999d5cf
PengTianxiang <932328811@qq.com>
2026-02-23 13:22:37 +0800

    20260223 First

--- Q2/Q2_Joint.py
+++ Q2/Q2_Joint.py
@@ -1,5 +1,7 @@
 import numpy as np
 import roboticstoolbox as rtb
+import torch
+from torch import nn
 
 class Joint:
     def __init__(self, Link_twist, Link_length, Link_offset, Joint_angle=0,print_require = True):
@@ -14,6 +16,28 @@ class Joint:
             [np.cos(self.Joint_angle), -np.sin(self.Joint_angle), 0, self.Link_length],
             [np.sin(self.Joint_angle)*np.cos(self.Link_twist), np.cos(self.Joint_angle)*np.cos(self.Link_twist), -np.sin(self.Link_twist), -self.Link_offset*np.sin(self.Link_twist)],
             [np.sin(self.Joint_angle)*np.sin(self.Link_twist), np.cos(self.Joint_angle)*np.sin(self.Link_twist), np.cos(self.Link_twist), self.Link_offset*np.cos(self.Link_twist)],
+            [0, 0, 0, 1]
+        ])
+        if self.print_require ==True:
+            print(matrix)
+        return matrix
+    
+    def assemble_joint(self):
+        return rtb.RevoluteMDH(alpha=self.Link_twist, a=self.Link_length, d=self.Link_offset)
+    
+class Joint_torch:
+    def __init__(self, Link_twist, Link_length, Link_offset, Joint_angle=0,print_require = True):
+        self.Link_twist = torch.deg2rad(Link_twist)
+        self.Link_length = Link_length
+        self.Link_offset = Link_offset
+        self.Joint_angle = torch.deg2rad(Joint_angle)
+        self.print_require = print_require
+
+    def trans_matrix(self):
+        matrix = torch.tensor([
+            [torch.cos(self.Joint_angle), -torch.sin(self.Joint_angle), 0, self.Link_length],
+            [torch.sin(self.Joint_angle)*torch.cos(self.Link_twist), torch.cos(self.Joint_angle)*torch.cos(self.Link_twist), -torch.sin(self.Link_twist), -self.Link_offset*torch.sin(self.Link_twist)],
+            [torch.sin(self.Joint_angle)*torch.sin(self.Link_twist), torch.cos(self.Joint_angle)*torch.sin(self.Link_twist), torch.cos(self.Link_twist), self.Link_offset*torch.cos(self.Link_twist)],
             [0, 0, 0, 1]
         ])
         if self.print_require ==True:
--- Q2/Q2_thinking.py
+++ Q2/Q2_thinking.py
@@ -1,18 +1,18 @@
 import torch
 from torch import nn
 import numpy as np
-from Q2 import Joint
+from Q2_Joint import Joint_torch
 
 """定义前向传播函数"""
-def frontward(Joint_matrix, joint_angle):
-    for angle in joint_angle:
-        angle.requires_grad_(True)
+def frontward(joint_angle):
 
-    shoulder_pan = Joint(0, 0, 0.0624, joint_angle[0])
-    shoulder_lift = Joint(-90, 0.035, 0, joint_angle[1])
-    elbow_flex = Joint(0, 0.116, 0, joint_angle[2])
-    wrist_flex = Joint(0, 0.135, 0, joint_angle[3])
-    wrist_roll = Joint(-90, 0, 0.061, joint_angle[4] )
+    joint_angle.requires_grad_(True)
+
+    shoulder_pan = Joint_torch(0, 0, 0.0624, joint_angle[0])
+    shoulder_lift = Joint_torch(-90, 0.035, 0, joint_angle[1])
+    elbow_flex = Joint_torch(0, 0.116, 0, joint_angle[2])
+    wrist_flex = Joint_torch(0, 0.135, 0, joint_angle[3])
+    wrist_roll = Joint_torch(-90, 0, 0.061, joint_angle[4] )
 
     shoulder_pan_trans = shoulder_pan.trans_matrix()
     shoulder_lift_trans = shoulder_lift.trans_matrix()
@@ -21,19 +21,16 @@ def frontward(Joint_matrix, joint_angle):
     wrist_roll_trans = wrist_roll.trans_matrix()
 
     final_matrix = shoulder_pan_trans@shoulder_lift_trans@elbow_flex_trans@wrist_flex_trans@wrist_roll_trans
-
-#定义损失函数
-def loss(current_position, target_position):
-    result_matrix = (target_position - current_position)*(target_position - current_position)*0.5
-    return torch.sum(result_matrix)
-
+    return final_matrix
 
 #current_position:各个关节当下角度  target_position：目标位置
-def gradient_decent(joint_angle, target_position, lr, epoches):
-
-    for epoch in epoches:
+def gradient_decent(joint_angle, target_position,epoches ,lr, loss):
+    optimzer = torch.optim.SGD([joint_angle], lr = lr)
+    for epoch in range(epoches):
+        optimzer.zero_grad()
         current_position = frontward(joint_angle)
         punish = loss(current_position, target_position)
-        gradient_matrix = punish.backward()
-        joint_angle = joint_angle - lr*gradient_matrix
+        punish.backward()
+        optimzer.step()
     return joint_angle
+
