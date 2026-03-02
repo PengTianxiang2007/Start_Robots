9d4371110fd7530594d9a9880c2e668502e19f89
PengTianxiang <932328811@qq.com>
2026-03-02 11:12:42 +0800

    20260302 Second Time

--- Q3/camCalibration_zyx/Hand_in_eye.py
+++ Q3/camCalibration_zyx/Hand_in_eye.py
@@ -31,8 +31,8 @@ def split_matrixes(big_matrix, rot_matrix_list, trs_matrix_list):
 "由3*3旋转矩阵和3*1平移矩阵拼接4*4变换矩阵"
 def splice_matrix(rot_matrix, trs_matrix):
     output = np.eye(4)
-    output[:3, :3] = final_rotation_matrix
-    output[:3, 3] = final_translation_matrix.reshape(3)
+    output[:3, :3] = rot_matrix
+    output[:3, 3] = trs_matrix.reshape(3)
     return output
 
 """读取json文件中base->gripper矩阵,并切割添加在相应列表中"""
@@ -84,31 +84,60 @@ def get_cb_matrixes(paths, rot_matrixes_list, trs_matrixes_list, camera_matrix, 
         trs_matrixes_list.append(img_translation)
 
 
+def get_extrinsics_hand_matrix():
+    rot_bt = [] #3*3旋转矩阵  base->gripper
+    trs_bt = [] #3*1平移矩阵  camera->board
+    rot_cb = []
+    trs_cb = []
 
-rot_bt = [] #3*3旋转矩阵  base->gripper
-trs_bt = [] #3*1平移矩阵  camera->board
-rot_cb = []
-trs_cb = []
+    """得到 base->gripper 矩阵"""
+    current_dir = Path(__file__).parent
+    file_path = current_dir / "data_20251224_handineye" / "captures.json"
+    get_bg_matrixes(file_path, rot_bt, trs_bt)
+    rot_bt = np.array(rot_bt)
+    trs_bt = np.array(trs_bt)
 
-"""得到 base->gripper 矩阵"""
-current_dir = Path(__file__).parent
-file_path = current_dir / "data_20251224_handineye" / "captures.json"
-get_bg_matrixes(file_path, rot_bt, trs_bt)
-rot_bt = np.array(rot_bt)
-trs_bt = np.array(trs_bt)
+    """得到 camera 参数矩阵"""
+    camera_data_path = current_dir / "data_20251224_handineye" / "target2cam_result.json"
+    camera_matrix, distCoeffs = get_camera_parameters(camera_data_path)
 
-"""得到 camera 参数矩阵"""
-camera_data_path = current_dir / "data_20251224_handineye" / "target2cam_result.json"
-camera_matrix, distCoeffs = get_camera_parameters(camera_data_path)
+    """得到 camera->board矩阵"""
+    images_path = current_dir / "data_20251224_handineye" / "images"
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    get_cb_matrixes(images_path, rot_cb, trs_cb, camera_matrix, distCoeffs, criteria)
+    rot_cb = np.array(rot_cb)
+    trs_cb = np.array(trs_cb)
 
-"""得到 camera->board矩阵"""
-images_path = current_dir / "data_20251224_handineye" / "images"
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-get_cb_matrixes(images_path, rot_cb, trs_cb, camera_matrix, distCoeffs, criteria)
-rot_cb = np.array(rot_cb)
-trs_cb = np.array(trs_cb)
+    """得到最终返回矩阵"""
+    final_rotation_matrix, final_translation_matrix = cv2.calibrateHandEye(rot_bt, trs_bt, rot_cb, trs_cb)
+    output = splice_matrix(final_rotation_matrix, final_translation_matrix)
+    return output
 
-"""得到最终返回矩阵"""
-final_rotation_matrix, final_translation_matrix = cv2.calibrateHandEye(rot_bt, trs_bt, rot_cb, trs_cb)
-output = splice_matrix(final_rotation_matrix, final_translation_matrix)
-print(output)
+def get_extrinsics_head_matrix():
+    rot_bt = [] #3*3旋转矩阵  base->gripper
+    trs_bt = [] #3*1平移矩阵  camera->board
+    rot_cb = []
+    trs_cb = []
+
+    """得到 base->gripper 矩阵"""
+    current_dir = Path(__file__).parent
+    file_path = current_dir / "data_20251229_handtoeye" / "captures.json"
+    get_bg_matrixes(file_path, rot_bt, trs_bt)
+    rot_bt = np.array(rot_bt)
+    trs_bt = np.array(trs_bt)
+
+    """得到 camera 参数矩阵"""
+    camera_data_path = current_dir / "data_20251229_handtoeye" / "target2cam_result.json"
+    camera_matrix, distCoeffs = get_camera_parameters(camera_data_path)
+
+    """得到 camera->board矩阵"""
+    images_path = current_dir / "data_20251229_handtoeye" / "images"
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    get_cb_matrixes(images_path, rot_cb, trs_cb, camera_matrix, distCoeffs, criteria)
+    rot_cb = np.array(rot_cb)
+    trs_cb = np.array(trs_cb)
+
+    """得到最终返回矩阵"""
+    final_rotation_matrix, final_translation_matrix = cv2.calibrateHandEye(rot_bt, trs_bt, rot_cb, trs_cb)
+    output = splice_matrix(final_rotation_matrix, final_translation_matrix)
+    return output
